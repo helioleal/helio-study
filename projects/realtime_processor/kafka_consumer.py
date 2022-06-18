4ee562e451e29bcf1dde650a1da07f361e896ab1
helioleal <42657605+helioleal@users.noreply.github.com>
2022-06-18 13:19:42 -0300

    Add files via upload

---
+++ projects/realtime_processor/kafka_consumer.py
@@ -0,0 +1,10 @@
+import service.yaml_service as yaml # Inner
+import service.avro_service as avro # Inner
+from kafka import KafkaConsumer
+
+config = yaml.read_yaml('file/config')
+
+consumer = KafkaConsumer(config['kafka_topic'])
+for message in consumer:
+    result = avro.decode(config['avro_schema'], message.value)
+    print(result)
